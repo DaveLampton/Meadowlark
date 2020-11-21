84433cd0c58110e55ee8350cf5ae029949ca4cff
https://github.com/DaveLampton <298615+DaveLampton@users.noreply.github.com>
2020-11-20 21:02:19 -0800

    added QA facilities

--- meadowlark.js
+++ meadowlark.js
@@ -13,27 +13,44 @@ app.set("port", process.env.PORT || 3000);
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function (req, res) {
+app.use((req, res, next) => {
+  res.locals.showTests =
+    app.get("env") != "production" && req.query.test === "1";
+  next();
+});
+
+app.get("/", (req, res) => {
   res.render("home");
 });
-app.get("/about", function (req, res) {
-  res.render("about", { fortune: fortune.getFortune() });
+app.get("/about", (req, res) => {
+  res.render("about", {
+    fortune: fortune.getFortune(),
+    pageTestScript: "/qa/tests-about.js",
+  });
+});
+app.get("/tours/hood-river", (req, res) => {
+  res.render("tours/hood-river");
+});
+app.get("/tours/request-group-rate", (req, res) => {
+  res.render("tours/request-group-rate");
 });
 
 // 404 catch-all handler (middleware)
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.status(404);
   res.render("404");
+  next();
 });
 
 // 500 error handler (middleware)
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500);
   res.render("500");
+  next();
 });
 
-app.listen(app.get("port"), function () {
+app.listen(app.get("port"), () => {
   console.log(
     "Express started on http://localhost:" +
       app.get("port") +
